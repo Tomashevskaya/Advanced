18b5b0a86c02e04a1c5e60163dca6143b140c079
Tomashevskaya <Dasha.tomashewsckaya@yandex.ru>
2018-11-27 00:31:37 +0300

    Operators overloading

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c1 = new FuelCar { Engine = 23 };
+            var c2 = new FuelCar { Engine = 45 };
+            bool compare = c1 > c2;
+            Console.WriteLine(compare); 
+            Console.ReadKey();
+        }
+    }
+}
