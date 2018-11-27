4a7eab48a4f89227e05b25436951bc3df3bdf0f5
Tomashevskaya <Dasha.tomashewsckaya@yandex.ru>
2018-11-27 13:31:04 +0300

    Text Printer

---
+++ TextPrint/TextPrint/Printer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextPrint
+{
+    public abstract class Printer
+    {
+        public abstract void Print(string text);
+    }
+}
